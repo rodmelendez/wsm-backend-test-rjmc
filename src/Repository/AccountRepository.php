e1c2cafe2d44511652cc9ee3e0e23f5f9d7027a0
rodmelendez <melendezr97@gmail.com>
2022-05-15 18:07:26 -0600

    Configuration template

--- src/Repository/AccountRepository.php
+++ src/Repository/AccountRepository.php
@@ -18,4 +18,29 @@ class AccountRepository extends DocumentRepository
             ->getQuery()
             ->execute();
     }
+    public function findAccountActive()
+    {
+        return $this->createQueryBuilder()
+            ->match()->field('status')  ->equals('ACTIVE')
+            ->getQuery()
+            ->execute();
+    }
+
+    public function getReports( string | null $accountId = null)
+    {
+        $dm = $this->getDocumentManager();
+        $builder = $dm->createAggregationBuilder(Account::class);
+        $builder->match()->field('status')  ->equals('ACTIVE')
+            ->lookup('Metrics')             ->foreignField('accountId')
+            ->localField('accountId')    ->alias('metrics')
+            ->group()                            ->field('id')
+            ->expression('$accountId')      ->field('spend')
+            ->sum( $builder->expr()->sum('$metrics.spend'))
+            ->field('impressions') ->sum( $builder->expr() ->sum('$metrics.impressions'))
+            ->field('clicks')->sum( $builder->expr()->sum('$metrics.clicks'));
+
+        $result = $builder->getAggregation()->getIterator()->toArray();
+
+        return $result;
+    }
 }
